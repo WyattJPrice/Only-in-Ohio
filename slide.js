0a23534531058edc66fd0ab9f68a93a4bf20f092
Aerolix_school <79605302+Aerolixdev@users.noreply.github.com>
2022-11-15 08:54:23 -0600

    redid

--- slide.js
+++ slide.js
@@ -1,11 +1,11 @@
 const popup = document.getElementsByClassName('popup');
 
 function info(); {
-  popup.showModal();
+   popup.showModal();
 }
 
 function exit(); {
-  popup.close();
+   popup.close();
 }
 
 
@@ -13,18 +13,22 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
-  var vid = document.getElementById("video");
-  vid.pause();
+   showDivs(slideIndex += n);
+   var vid = document.getElementById("video");
+   vid.pause();
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+   var i;
+   var x = document.getElementsByClassName("mySlides");
+   if (n > x.length) {
+      slideIndex = 1
+   }
+   if (n < 1) {
+      slideIndex = x.length
+   };
+   for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+   }
+   x[slideIndex - 1].style.display = "block";
 }
